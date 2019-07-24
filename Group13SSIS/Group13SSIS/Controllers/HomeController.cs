5bbb1f3b3c2c97b7f4c283778aa837faa7cf17f8
TimeSpeeding <andrewzhu0328@163.com>
2019-07-24 10:36:49 +0800

    database update

---
+++ Group13SSIS/Group13SSIS/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Group13SSIS.Models;
+using Group13SSIS.Utility;
+
+namespace Group13SSIS.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Register Page
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        // POST: for Admin to add new user
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Group13SSIS/Group13SSIS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Group13SSIS.MvcApplication" Language="C#" %>
